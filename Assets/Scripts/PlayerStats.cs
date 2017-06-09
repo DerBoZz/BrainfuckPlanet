9aff30eab8f19bbea3eae65b56384917dc04f813
Nightfall <julius.krueger@hotmail.de>
2017-06-09 22:06:37 +0200

    PlayerStats script

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class PlayerStats {
+
+    public static int playerHealth;
+
+    public static int playerScore;
+
+    //public static Weapon [] weaponArray;
+
+    public static int currency;
+
+
+}
