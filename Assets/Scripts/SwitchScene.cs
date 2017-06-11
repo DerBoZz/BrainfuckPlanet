f2b7925f0c59e688826f14270cbb2773d308c5e5
Marcel Zurawka <marcel.zurawka@web.de>
2017-06-11 11:01:39 +0200

    SceneSwitcher

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SwitchScene : MonoBehaviour {
+
+    float timer;
+	// Use this for initialization
+	void Start () {
+        timer = 10f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            int index = SceneManager.GetActiveScene().buildIndex;
+            index = (index + 1) % SceneManager.sceneCount;
+            SceneManager.LoadScene(index);
+        }
+	}
+}
