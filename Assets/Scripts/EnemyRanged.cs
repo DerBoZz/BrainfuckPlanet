7e744879c4373c7472471d7c000ac2a267afc1ad
Nightfall <julius.krueger@hotmail.de>
2017-06-11 08:15:49 +0200

    Ranged Enemy stuff

--- Assets/Scripts/EnemyRanged.cs
+++ Assets/Scripts/EnemyRanged.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyRanged : MonoBehaviour {
+public class EnemyRanged : Enemy {
+
+    private Animator anim;
+    public GameObject projectile;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +17,27 @@ public class EnemyRanged : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+        
+    {
+        if(collision.tag == "Player")
+        {
+            float angle = Vector3.Angle(transform.position, collision.transform.position);
+            float distance = gameObject.GetComponent<CircleCollider2D>().radius;
+            gameObject.GetComponent<CircleCollider2D>().enabled = false;
+            RaycastHit2D raycast = Physics2D.Raycast(transform.position, collision.transform.position-transform.position, distance);
+            gameObject.GetComponent<CircleCollider2D>().enabled = true;
+            if (raycast.collider != null && raycast.collider.tag == "Player")
+            {
+                GameObject bullet = Instantiate(projectile, transform.position,Quaternion.identity);
+                bullet.transform.Translate((raycast.point - new Vector2(transform.position.x, transform.position.y)).normalized * 1.0f);
+                bullet.transform.rotation = Quaternion.LookRotation(bullet.transform.position);
+            }
+        }
+        
+
+        
+    }
+
 }
